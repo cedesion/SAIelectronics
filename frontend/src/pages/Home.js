79e69dc26290ef7d9523b64f28d9ea5d24235e68
Raghav <raghav03.doda@gmail.com>
2023-03-07 13:50:22 +0530

    Offers slider added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar';
+import Offers from '../components/Offers';
 
 const Home = () => {
   return (
     <>
         <Navbar/>
+        <Offers/>
     </>
   )
 }
