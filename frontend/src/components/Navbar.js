3bf6fb7e8540dc33f8fd824baff07bee04e27bcd
priyanshuraj30 <priyanshuraj30122003@gmail.com>
2023-03-08 23:03:33 +0530

    nav bar UI updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,13 +5,19 @@ const  Navbar = () =>{
     return(
         <header>
             <div className="container">
-            <a href="/" style={{color: "white" , fontSize:"200%"}}><CgProfile style={{color: "white"}}/>  SAI Electronics</a>
+            <div className="containertitle">
+            <a href="/" style={{color: "white"}}>  SAI Electronics</a>
+            </div>
             <div className="search">
-            <AiOutlineSearch/>
-            <input type="text" placeholder=  "Search here" className="searchBar"/>
+            <input type="text" placeholder=  "Search here..." className="searchBar"/>
+            <div className="searchhereicon">
+                <span>
+                    <AiOutlineSearch/>
+                </span>  
+            </div>
             </div>
                 <nav>
-                        <div>
+                        <div className="navend" style={{fontSize:"115%"}}>
                             <a href="/companies" style={{color: "white"}}>Companies</a>
                             <a href="/services" style={{color: "white"}}>Services</a>
                             <a href="/" className="icon"><CgProfile/></a>
