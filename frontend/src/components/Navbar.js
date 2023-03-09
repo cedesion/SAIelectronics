60ee308db15b103537d6011154ff574d5e7b98b6
priyanshuraj30 <priyanshuraj30122003@gmail.com>
2023-03-09 12:40:20 +0530

    navbar ui updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,26 +1,25 @@
 import {CgProfile} from "react-icons/cg";
 import {AiOutlineSearch} from "react-icons/ai"
+import {AiOutlineShoppingCart} from "react-icons/ai"
 const  Navbar = () =>{
 
     return(
         <header>
             <div className="container">
             <div className="containertitle">
-            <a href="/" style={{color: "white"}}>  SAI Electronics</a>
+            <a href="/" style={{color: "white"}}><span id="profile"><a href="/" className="icon"><CgProfile/></a></span>  SAI Electronics</a>
             </div>
             <div className="search">
-            <input type="text" placeholder=  "Search here..." className="searchBar"/>
-            <div className="searchhereicon">
-                <span>
-                    <AiOutlineSearch/>
-                </span>  
-            </div>
+                <input  className="searchbar" placeholder="Search here...."/>
+                <button className="searchicon">
+                    <AiOutlineSearch />
+                </button>
             </div>
                 <nav>
                         <div className="navend" style={{fontSize:"115%"}}>
                             <a href="/companies" style={{color: "white"}}>Companies</a>
                             <a href="/services" style={{color: "white"}}>Services</a>
-                            <a href="/" className="icon"><CgProfile/></a>
+                            <a href="/" style={{color: "white", fontSize:"120%"}}><AiOutlineShoppingCart/></a>
                         </div>
                     
                 </nav>
